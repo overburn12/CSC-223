446661074884b6463471c00b83417a8b49f6b26e
Nicholas Kane <overburn12@gmail.com>
2020-10-14 12:57:26 -0400

    fixed spelling

--- Chapter 8/Ch8-1.cpp
+++ Chapter 8/Ch8-1.cpp
@@ -1,9 +1,9 @@
 /*
 (Game: hangman) Write a hangman game that randomly generates a word and prompts the user 
-to guess one letter at a time, as showm in the sample run. Each letter in the word is 
+to guess one letter at a time, as shown in the sample run. Each letter in the word is 
 displayed as an asterisk. When the user makes a correct guess, the actual letter is then 
 displayed. When the user finishes a word, display the number of misses and ask the user 
-whether to continue to play with another word. Declare an arroay to store words, 
+whether to continue to play with another word. Declare an array to store words, 
 as follow: Store 20 different words in a text file, read them into an array. Randomly 
 pick a number 1..20 and use this word for the current round.
 */
