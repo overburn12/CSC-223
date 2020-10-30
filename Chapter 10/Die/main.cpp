addf9373b08cee3bb8082e056a1e652949a7c6ba
Nicholas Kane <overburn12@gmail.com>
2020-10-30 13:22:30 -0400

    added comments

--- Chapter 10/Die/main.cpp
+++ Chapter 10/Die/main.cpp
@@ -21,38 +21,59 @@ are two three's, the two five's make up the better result.)
 int main(){
     die test_die;
     int roll_test_count, highest_index, words_index;
-    int word_index_count[6] = {0};
-    int die_roll_count[6] = {0};
-    std::string words[6] = {"Single roll", "Two of a kind", "Three of a kind", 
-                           "Four of a kind", "Five of a kind", "Perfect roll"};
+    
+    //statistic counting, how many times 2 of a kind, 3 of a kind, 4 of a kind etc.. occurs
+    int word_index_count[5] = {0};
+
+    //count for how many times each face value of the die occurs, 0-based index 
+    int die_roll_count[6] = {0}; 
+
+    //if the face value of the die occured x amount of times then its called: two of a kind, 3 of a kind, etc..
+    std::string words[5] = {"Single roll", "Two of a kind", "Three of a kind", 
+                           "Four of a kind", "Five of a kind"};
 
     std::cout << "How many times would you like to run the roll test? ";
     std::cin >> roll_test_count;
 
     for(int x = 0; x < roll_test_count; x++){
+        //reset the counts to zero
         for(int i = 0; i < 6; i++)
             die_roll_count[i] = 0;
         highest_index = 0;
 
+        //conduct a single test of 5 rolls
         std::cout << "Roll: ";
         for(int i = 0; i < 5; i++){
             test_die.Roll();
             std::cout << test_die.Show();
+            //add the face value of a single roll to the face value count
             die_roll_count[test_die.Show()-1]++;
         }
 
-        for(int i = 5; i >= 0; i--)
-            if(die_roll_count[highest_index] * (highest_index + 1) < die_roll_count[i] * (i + 1) )
+        //loop through the counts for each face of the die
+        for(int i = 0; i < 6; i++)
+            /*compare the face value of the side multiped by the occurence of the side
+             with the current highest combo*/
+            if(die_roll_count[highest_index] * (highest_index + 1) <= die_roll_count[i] * (i + 1) )
                 highest_index = i;
 
+        //the index for the wording is zero-based
+        //"single roll" = 0, "2 of a kind" = 1, "3 of a kind" = 2 etc..
         words_index = die_roll_count[highest_index] - 1;
+
+        //statistic counting, how many times did 2 of a kind, 3 of a kind etc occur during all the tests
         word_index_count[words_index]++;
+
+        //output the naming of type of occurence for the highest face value combo
         std::cout << " Best result: " << words[words_index] << ": " << highest_index + 1 << std::endl;
+
+        if(words_index==4) std::cout << "!!!!!!!!!!!!!!!!!!!!!! 5 of  a kind !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"; 
+
     }
 
     if(roll_test_count > 20){
         std::cout << "Statistic occurence:\n" << std::fixed << std::setprecision(2);
-            for(int i = 0; i < 6; i++){
+            for(int i = 0; i < 5; i++){
                 std::cout << words[i] << " happend " << word_index_count[i] << " times, or ";
                 std::cout << ( (double)word_index_count[i] / (double)roll_test_count) * 100.0 <<"%\n";
             }
