ff601d1dcbf2fbd44b906676a2960b3bd179da7b
Nicholas Kane <overburn12@gmail.com>
2020-11-12 01:35:06 -0500

    update PointerList class

--- Chapter 12/PointerList.cpp
+++ Chapter 12/PointerList.cpp
@@ -4,13 +4,15 @@ class PointerList{
 private:
     T * the_list;
     int list_size;
+    int block_size;
+    int num_blocks;
 
 public:
     PointerList(){
         list_size = 0;
-        the_list = new T[list_size];
-        for (int i = 0; i < list_size; i++)
-            the_list[i] = 0;
+        num_blocks = 1;
+        block_size = 10;
+        the_list = new T[block_size * num_blocks];
     }
 
     ~PointerList(){
@@ -18,14 +20,26 @@ public:
     }
 
     void add(T value){
-        T * tmp = new T[list_size + 1];
-        for (int i = 0; i < list_size; i++){
-            tmp[i] = the_list[i];
+        if (list_size == block_size * num_blocks)
+        {
+            num_blocks += 1;
+            T * tmp = new T[block_size * num_blocks];
+            for (int i = 0; i < list_size; i++)
+            {
+                tmp[i] = the_list[i];
+            }
+            delete[] the_list;
+            the_list = tmp;
         }
-        list_size++;
-        delete[] the_list;
-        the_list = tmp;
-        the_list[list_size - 1] = value;
+        the_list[list_size] = value;
+        list_size += 1;
+    }
+
+    void clear(){
+        delete [] the_list;
+        list_size = 0;
+        num_blocks = 1;
+        the_list = new T[block_size * num_blocks];
     }
 
     bool is_empty(){
