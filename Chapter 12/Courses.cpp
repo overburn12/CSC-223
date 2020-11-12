28df730142f95fce07b4cf792c0b0955bcb1bbc5
Nicholas Kane <overburn12@gmail.com>
2020-11-12 18:34:08 -0500

    updated code

--- Chapter 12/Courses.cpp
+++ Chapter 12/Courses.cpp
@@ -2,64 +2,82 @@
 #include <iostream>
 #include <iomanip>
 
-class Course{
+class Course
+{
 protected:
-    int courseID;
+    std::string courseID;
     std::string courseTitle;
-    int roomNumber;
-    int contactHours;
+    std::string roomNumber;
+    std::string contactHours;
 public:
-    Course( int new_courseID, std::string new_courseTitle, int new_roomNumber, int new_contactHours ) : 
-            courseID(new_courseID), courseTitle(new_courseTitle), 
-            roomNumber(new_roomNumber), contactHours(new_contactHours) {}
+    Course( std::string new_courseID, 
+            std::string new_courseTitle, 
+            std::string new_roomNumber, 
+            std::string new_contactHours ) 
+        : courseID( new_courseID ), 
+          courseTitle( new_courseTitle ), 
+          roomNumber( new_roomNumber ), 
+          contactHours( new_contactHours ) {}
 
-    Course() : courseID(0), courseTitle(""), roomNumber(0), contactHours(0) {}
+    Course() : courseID(""), courseTitle(""), roomNumber(""), contactHours("") {}
 
     virtual void show() = 0 ;
 };
 
-class CirriculumClass : public Course{
+class CirriculumClass : public Course
+{
     double creditHours;
     double tuitionRate;
 public:
-    CirriculumClass( int new_courseID, std::string new_courseTitle, int new_roomNumber, 
-                     int new_contactHours, double new_creditHours, double new_tuitionRate ) : 
-                     Course(new_courseID, new_courseTitle, new_roomNumber, new_contactHours) ,
-                     creditHours(new_creditHours), 
-                     tuitionRate(new_tuitionRate) {}
+    CirriculumClass( std::string new_courseID, 
+                     std::string new_courseTitle, 
+                     std::string new_roomNumber, 
+                     std::string new_contactHours, 
+                     double new_creditHours, 
+                     double new_tuitionRate ) 
+            : Course( new_courseID, new_courseTitle, new_roomNumber, new_contactHours ), 
+              creditHours( new_creditHours ), 
+              tuitionRate( new_tuitionRate ) {}
 
     CirriculumClass() : Course(), creditHours(0), tuitionRate(0) {}
 
     void show(){
         int sp = 30;
-        std::cout << std::fixed << std::setprecision(2) << std::endl <<
-        std::setw(30) << "[Cirriculum Class]" << std::endl <<
-        std::setw(sp) << "Course title: " << courseTitle << std::endl <<
-        std::setw(sp) << "Course ID: " << courseID << std::endl <<
-        std::setw(sp) << "Room number: " << roomNumber << std::endl <<
-        std::setw(sp) << "Credit hours: " << creditHours << std::endl <<
-        std::setw(sp) << "Tuition per credit hour: " << tuitionRate << std::endl <<
-        std::setw(sp) << "Course fee: " << tuitionRate * creditHours << std::endl;
+        std::cout << std::fixed << std::setprecision(2) << '\n' <<
+        std::setw(34) << "[Cirriculum Class]" << '\n' <<
+        std::setw(sp) << "Course title: "     << courseTitle  << '\n' <<
+        std::setw(sp) << "Course ID: "        << courseID     << '\n' <<
+        std::setw(sp) << "Room number: "      << roomNumber   << '\n' <<
+        std::setw(sp) << "Contacts hours: "   << contactHours << '\n' <<
+        std::setw(sp) << "Credit hours: "     << creditHours  << '\n' <<
+        std::setw(sp) << "Tuition rate: "     << tuitionRate  << '\n' <<
+        std::setw(sp) << "Course fee: "       << tuitionRate * creditHours << std::endl;
     }
 };
 
-class ContinuingEducation : public Course{
+class ContinuingEducation : public Course
+{
     double fee;
 public:
-    ContinuingEducation( int new_courseID, std::string new_courseTitle, int new_roomNumber, 
-                         int new_contactHours, double new_fee) :
-                         Course(new_courseID, new_courseTitle, new_roomNumber, new_contactHours), 
-                         fee(new_fee) {}
+    ContinuingEducation( std::string new_courseID, 
+                         std::string new_courseTitle, 
+                         std::string new_roomNumber, 
+                         std::string new_contactHours, 
+                         double new_fee )
+            : Course( new_courseID, new_courseTitle, new_roomNumber, new_contactHours ), 
+              fee( new_fee ) {}
 
     ContinuingEducation() : Course(), fee(0) {}
 
-    void show(){
+    void show()
+    {
         int sp = 30;
-        std::cout << std::fixed << std::setprecision(2) << std::endl <<
-        std::setw(35) << "[Continuing Education Course]" << std::endl <<
-        std::setw(sp) << "Course title: " << courseTitle << std::endl <<
-        std::setw(sp) << "Course ID: " << courseID << std::endl <<
-        std::setw(sp) << "Room number: " << roomNumber << std::endl <<
-        std::setw(sp) << "Course fee: " << fee << std::endl;
+        std::cout << std::fixed << std::setprecision(2) << '\n' <<
+        std::setw(42) << "[Continuing Education Course]" << '\n' <<
+        std::setw(sp) << "Course title: "   << courseTitle  << '\n' <<
+        std::setw(sp) << "Course ID: "      << courseID     << '\n' <<
+        std::setw(sp) << "Contacts Hours: " << contactHours << '\n' <<
+        std::setw(sp) << "Room number: "    << roomNumber   << '\n' <<
+        std::setw(sp) << "Course fee: "     << fee          << std::endl;
     }
 };
--- Chapter 12/main.cpp
+++ Chapter 12/main.cpp
@@ -13,9 +13,9 @@ A continuing education course has a flat fee (cost).
 Implement a show() function in every class that displays the data in the object.
 
 Part A: Create an inheritance-based collection of classes to represent these different courses. 
-Include code in main to test your work by instantiating objects of the two derived classes.
+Include code in main to new_class your work by instantiating objects of the two derived classes.
 
-Part B: Modify your program. Remove the objects you created in main to test the classes. Modify 
+Part B: Modify your program. Remove the objects you created in main to new_class the classes. Modify 
 the base class so that show() becomes a virtual function.
 
 In main, create a single array capable of holding pointers to objects of both curriculum and 
@@ -33,115 +33,184 @@ Implement each menu option
 */
 
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <limits>
 #include "Courses.cpp"
 
-void expand_list_one_more(Course** &classes, int &class_list_size)
+void add_course(Course** &classes, int &class_list_size, bool is_cirriculum_class);
+void do_menu(int selection, Course** &classes, int &class_list_size);
+void load_default_list(Course** &classes, int &class_list_size);
+double get_double();
+int get_menu();
+
+int main()
 {
-    Course** temp = new Course*[class_list_size+1];
-
-    for(int i = 0; i < class_list_size; i++)
+    int selection, class_list_size = 0;
+    Course** courses;
+    std::cout << std::endl;
+    do
     {
-        temp[i] = classes[i];
+        selection = get_menu();
+        do_menu(selection, courses, class_list_size);
     }
-
-    class_list_size += 1;
-    delete [] classes;
-    classes = temp;
+    while(selection != 9);
+    delete [] courses;
+    return 0;
 }
 
-void the_default_list(Course** &classes, int &class_list_size)
+void load_default_list(Course** &classes, int &class_list_size)
 {
     class_list_size = 6;
+    std::string default_contact = "M-F 10AM to 12PM";
     delete [] classes;
     classes = new Course*[class_list_size];
     //classes[] = new CirriculumClass( courseID, courseTitle, roomNumber, contactHours, creditHours, tuitionRate );
-    classes[0] = new CirriculumClass( 3012,  "Computers", 1, 10, 4, 325.00 );
-    classes[1] = new CirriculumClass( 6233, "Philosophy", 2, 10, 3, 325.00 );
-    classes[2] = new CirriculumClass( 2223,    "English", 3, 10, 3, 240.00 );
-    classes[3] = new CirriculumClass( 1212,       "Math", 4, 10, 3, 240.00 );
+    classes[0] = new CirriculumClass( "CSC-223", "Computer Programming C++", "11-306", default_contact, 4, 325.00 );
+    classes[1] = new CirriculumClass( "PHL-103", "Introduction to Ethics",  "6-422", default_contact, 3, 325.00 );
+    classes[2] = new CirriculumClass( "ENG-251", "Technical Writing", "12-105", default_contact, 3, 240.00 );
+    classes[3] = new CirriculumClass( "MTH-160", "Statistics I", "12-210", default_contact, 3, 240.00 );
     //classes[] = new ContinuingEducation( courseID, courseTitle, roomNumber, contactHours, fee );
-    classes[4] = new ContinuingEducation( 3333, "Paramedic", 5, 10, 500.00 );
-    classes[5] = new ContinuingEducation( 9999,   "Welding", 6, 10, 500.00 );
+    classes[4] = new ContinuingEducation( "PEC-250", "Fitness and Wellness", "5-105", default_contact, 500.00 );
+    classes[5] = new ContinuingEducation( "HED-209", "Drugs and Behavior", "5-203", default_contact, 500.00 );
 }
 
-void add_curriculum(Course** &classes, int &class_list_size)
-{
-    int courseID, roomNumber, contactHours;
-    double creditHours, tuitionRate;
-    std::string courseTitle;
+double get_double()
+{   /*
+        This function gets a string from std::cin
+        it then looks through and builds a decimal number
+        while skiping over any non-number charecters
+        and returns the number as a double.
+        If there is a '-' before any numbers have been computed
+        then the number returned will be negative.
+        this is to prevent the program from getting suck in an infinite loop
+        when someone enters a string for and expression like
+        'std::cin >> (double or int);'
 
-    std::cout << "Enter course ID: "; std::cin >> courseID;
-    std::cout << "Enter course Title: "; std::cin >> courseTitle;
-    std::cout << "Enter the room number: "; std::cin >> roomNumber;
-    std::cout << "Enter the # of contact hours: "; std::cin >> contactHours;
-    std::cout << "Enter the credit hours: "; std::cin >> creditHours;
-    std::cout << "Enter the tuition rate: "; std::cin >> tuitionRate;
+        Precondition: (user enters) getwwh-sfh23gds-dfntrw3.r34rfg5
+        Postcondition: -233.345
+        Precondition: (user enters) vewr8nv we3dfg45svf. fdh234ggs
+        Postcondition: 8345.234
+        Precondition: (user enters) efnaler jfsnldjrg  nslkjngd sljnfsdlewnfsj
+        Postcondition: 0
+    */
+    std::string input_line;
+    double the_int = 0, the_decimal = 0, the_decimal_position = 1;
+    bool before_decimal_point = true;
+    bool is_negative = false;
 
-    Course* test = new CirriculumClass( courseID, courseTitle, roomNumber, 
-                                        contactHours, creditHours, tuitionRate );
+    std::getline(std::cin, input_line);
 
-    expand_list_one_more(classes, class_list_size);
-    classes[class_list_size - 1] = test;
+    for(int i = 0; i < input_line.size(); i++)
+    {
+        if('0' <= input_line[i] && input_line[i] <= '9')
+        {
+            if(before_decimal_point)
+            {
+                the_int = (the_int * 10) + (input_line[i] - '0');
+            }
+            else
+            {
+                the_decimal += ((double)(input_line[i] - '0') / std::pow(10.0, the_decimal_position++));
+            }
+        }
+        else
+        {
+            if(input_line[i] == '-' && the_int == 0)
+            {
+                is_negative = true;
+            }
+            if(input_line[i] == '.')
+            {
+                before_decimal_point = false;
+            }
+        }
+    }
+    if(is_negative)
+    {
+        return -1 * (the_int + the_decimal);
+    }
+    return (the_int + the_decimal);  
 }
 
-void add_continuing(Course** &classes, int &class_list_size)
+void add_course(Course** &classes, int &class_list_size, bool is_cirriculum_class)
 {
-    int courseID, roomNumber, contactHours;
-    double fee;
-    std::string courseTitle;
+    std::string courseTitle, courseID, roomNumber, contactHours;
+    double creditHours, tuitionRate, fee;
+    Course* new_class;
 
-    std::cout << "Enter course ID: "; std::cin >> courseID;
-    std::cout << "Enter course Title: "; std::cin >> courseTitle;
-    std::cout << "Enter the room number: "; std::cin >> roomNumber;
-    std::cout << "Enter the # of contact hours: "; std::cin >> contactHours;
-    std::cout << "Enter the course fee: "; std::cin >> fee;
+    std::cout << "Enter course Title: "; std::getline(std::cin, courseTitle);
+    std::cout << "Enter course ID: "; std::getline(std::cin, courseID);
+    std::cout << "Enter the room number: "; std::getline(std::cin, roomNumber);
+    std::cout << "Enter the # of contact hours: "; std::getline(std::cin, contactHours);
 
-    Course* test = new ContinuingEducation( courseID, courseTitle, roomNumber, contactHours, fee );
+    if(is_cirriculum_class)
+    {
+        std::cout << "Enter the credit hours: "; creditHours = get_double();
+        std::cout << "Enter the tuition rate: "; tuitionRate = get_double();
+        new_class = new CirriculumClass( courseID, courseTitle, roomNumber, contactHours, creditHours, tuitionRate );
+    }
+    else
+    {
+        std::cout << "Enter the course fee: "; fee = get_double();
+        new_class = new ContinuingEducation( courseID, courseTitle, roomNumber, contactHours, fee );
+    }
+
+    //--------------expand the course pointer array 1 more------------------------
+    {
+        Course** temp = new Course*[class_list_size+1];
+        for(int i = 0; i < class_list_size; i++)
+        {
+            temp[i] = classes[i];
+        }
+        class_list_size += 1;
+        delete [] classes;
+        classes = temp;
+    }
+    //--------------------------------------------------------------------------
     
-    expand_list_one_more(classes, class_list_size);
-    classes[class_list_size - 1] = test;
+    classes[class_list_size - 1] = new_class;
 }
 
 int get_menu()
 {
     int selection;
     std::cout << "   [Class Program Main Menu]" << std::endl
-              << "0. Load a default pre-programmed array of courses" << std::endl
-              << "1. Add a curriculum class to schedule" << std::endl
-              << "2. Add a continuing education class to schedule" << std::endl
-              << "3. Display all records" << std::endl
-              << "4. Clear the list of classes" << std::endl
+              << "1. Load a default pre-programmed array of courses" << std::endl
+              << "2. Add a curriculum class to schedule" << std::endl
+              << "3. Add a continuing education class to schedule" << std::endl
+              << "4. Display all records" << std::endl
+              << "5. Clear the list of classes" << std::endl
               << "9. Exit" << std::endl
               << " > ";
-    std::cin >> selection;
+    selection = (int)get_double();
     return selection;
 }
 
 void do_menu(int selection, Course** &classes, int &class_list_size)
 {
     std::cout << std::endl;
-
     switch(selection)
     {
-        case 0: // Load a default pre-programmed array of courses
+        case 1: // Load a default pre-programmed array of courses
         {
             std::cout << "Loading the default preprogrammed class list" << std::endl;
-            the_default_list(classes, class_list_size);
+            load_default_list(classes, class_list_size);
         }
         break;
-        case 1: //Add a curriculum class to schedule
+        case 2: //Add a curriculum class to schedule
         {
             std::cout << "[Adding a new Cirriculum Class]" << std::endl;
-            add_curriculum(classes, class_list_size);
+            add_course(classes, class_list_size, true);
         } 
         break;
-        case 2: //Add a continuing education class to schedule
+        case 3: //Add a continuing education class to schedule
         {
             std::cout << "[Adding a new Continuing Education Class]" << std::endl;
-            add_continuing(classes, class_list_size);
+            add_course(classes, class_list_size, false);
         } 
         break;
-        case 3: //Display all records
+        case 4: //Display all records
         {
             std::cout << "Printing a list of all Classes" << std::endl;
             for(int i = 0; i < class_list_size; i++)
@@ -150,12 +219,12 @@ void do_menu(int selection, Course** &classes, int &class_list_size)
             }
         } 
         break;
-        case 4: //Clear the list of classes
+        case 5: //Clear the list of classes
         {
             std::cout << "Clearing the list of saved classes" << std::endl;
             delete [] classes;
             class_list_size = 0;
-            classes = new Course*[0];
+            classes = new Course*[class_list_size];
         }
         break;
         case 9:
@@ -169,23 +238,6 @@ void do_menu(int selection, Course** &classes, int &class_list_size)
         }
         break;
     }
-
     std::cout << std::endl;
 }
 
-int main()
-{
-    int selection, class_list_size = 0;
-    Course** courses;
-    std::cout << std::endl;
-    do
-    {
-        selection = get_menu();
-        do_menu(selection, courses, class_list_size);
-    }
-    while(selection != 9);
-
-    delete [] courses;
-
-    return 0;
-}
