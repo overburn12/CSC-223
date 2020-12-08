0bf8211dc2877ba27da1de01eb960342b2fa6b94
Nicholas Kane <overburn12@gmail.com>
2020-12-08 00:31:13 -0500

    updated

--- Chapter 14/main.cpp
+++ Chapter 14/main.cpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <vector>
 
-#define __PATH 'O' //not used
-#define __WALL '+'
-#define __START 'S' //not used
-#define __END 'E'
+#define __PATH 'O'
 #define __SOLVED '@'
+#define __WALL '+'
+
+#define __START 'S' 
+#define __END 'E'
 
 //used for storing the path coordinates
 struct point{ int x, y; };
@@ -22,7 +23,6 @@ struct maze_data{
         std::ifstream in_file (file_name);
 
         in_file >> size_y >> size_x >> start_y >> start_x;
-
         data = new char*[size_y];
 
         for(int y = 0; y < size_y; y++){
@@ -30,113 +30,110 @@ struct maze_data{
 
             for(int x = 0; x < size_x; x++)
                 in_file >> data[y][x];
-        }
-
-        in_file.close();    
-    }
-
-    //treat maze_data[][] as the data array itself
-    char* operator [] (int index){ 
-        return data[index]; 
+        }   
     }
 
     //automatic delete for the pointer array
-    ~maze_data(){ 
+    ~maze_data(){
         for(int y = 0; y < size_y; y++) 
             delete [] data[y]; 
-
-        delete [] data; 
+        delete [] data;
     }
 
     //print the maze to the terminal
     void print(){
+        std::string hbar (size_x, '-');
+        char corner = '+';
+        char vbar = '|';
+
+        std::cout << '\t' << corner << hbar << corner << std::endl;
         for(int y = 0; y < size_y; y++){
-            std::cout << '\t';
+            std::cout << '\t' << vbar;
 
             for(int x = 0; x < size_x; x++)
                 std::cout << data[y][x];
 
-            std::cout << '\n';
+            std::cout << vbar << '\n';
         }
+        std::cout << '\t' << corner << hbar << corner << std::endl;
     }
 
-    //draw the stored path on the map
-    void draw_path_on_map(){
+    //recursive algorithm to solve the maze path
+    //this function will solve for the shortest path, and store it to this->path
+    void solve(std::vector<point> current_path, int x, int y){
+
+        //exit if the coordinate is invalid
+        if(x < 0 || x > size_x - 1 || y < 0 || y > size_y - 1)
+            return;
+
+        //exit if the coordinate is a wall
+        if(data[y][x] == __WALL)
+            return;
+
+        //exit if the coordinate is already in the solve path
+        for(int i = 0; i < current_path.size(); i++)
+            if(current_path[i].x == x && current_path[i].y == y)
+                return;
+
+        //exit if the current path plus this coodrinate is longer or equal to the stored solve path
+        if(current_path.size() + 1 >= path.size() && path.size() != 0)
+            return;
+
+        //add this coordinate to the current solve path
+        current_path.push_back({x , y});
+
+        //if we have found the end of the path
+        if(data[y][x] == __END){
+            //store the path with the maze
+            path = current_path;
+            return;
+        }
+
+        //branch in each direction
+        solve(current_path, x, y + 1); // down
+        solve(current_path, x + 1, y); // right
+        solve(current_path, x - 1, y); // left
+        solve(current_path, x, y - 1); // up
+    }
+
+    //the starting function for initiating the recursive solve algorithm
+    void solve(){
+        //a blank starting point for building the solve algorithm path
+        std::vector<point> current_path;
+
+        //the starting point of the recursive solve algorithm
+        solve(current_path, start_x, start_y);
+
+        //fill in the map with the solve path
         for(int i = 1; i < path.size() - 1; i++)
             data[ path[i].y ][ path[i].x ] = __SOLVED;
     }
 };
 
-void solve(maze_data &maze, std::vector<point> path, int x, int y, bool finding_shortest_path){
-
-    //exit if we already found a path and we're not looking for the shortest path
-    if(maze.path.size() != 0 && !finding_shortest_path) 
-        return;
-    
-    //exit if the coordinates are invalid
-    if(x < 0 || x > maze.size_x - 1 || y < 0 || y > maze.size_y - 1)
-        return;
-
-    //exit if the coordinates is a wall
-    if(maze[y][x] == __WALL)
-        return;
-
-    //exit if the coordinates are already in the solve path
-    for(int i = 0; i < path.size(); i++)
-        if(path[i].x == x && path[i].y == y)
-            return;
-
-    //add this coordinate to the current solve path
-    path.push_back({x , y});
-
-    //exit if the current path is longer or equal to the stored solve path
-    if(path.size() >= maze.path.size() && maze.path.size() != 0)
-        return;
-
-    //if we have found the end of the path
-    if(maze[y][x] == __END){
-        std::cout << "Path found, length: " << path.size() << std::endl;
-
-        //store the path with the maze
-        maze.path = path;
-
-        return;
-    }
-
-    //branch in each direction
-    solve(maze, path, x, y + 1, finding_shortest_path); // down
-    solve(maze, path, x + 1, y, finding_shortest_path); // right
-    solve(maze, path, x - 1, y, finding_shortest_path); // left
-    solve(maze, path, x, y - 1, finding_shortest_path); // up
-}
 
 int main(){
     std::string file_name;
-    
-    std::cout << "Enter maze file to solve: ";
+
+    std::cout << "Enter a file name: ";
     std::cin >> file_name;
 
-    std::ifstream test_file (file_name);
-    if(test_file.fail()){
-        std::cout << "File has failed!" << std::endl;
-        test_file.close();
+    //exit if the file cannot be opened
+    std::ifstream test_file(file_name);
+    if(!test_file.good()){
+        std::cout << "File not good";
         return 0;
     }
     test_file.close();
 
-    //the data for the maze layout
+    //the maze object
     maze_data maze(file_name);
 
-    //a blank starting point for building the solve algorithm path
-    std::vector<point> path;
-
-    std::cout << "\nUNSOLVED:\n";
+    std::cout << "UNSOLVED:" << std::endl;
     maze.print();
 
-    solve(maze, path, maze.start_x, maze.start_y, true);
-    maze.draw_path_on_map();
+    maze.solve();
 
-    std::cout << "\nSOLVED:\n";
+    std::cout << "SOLVED:" << std::endl;
     maze.print();
 
     return 0;
